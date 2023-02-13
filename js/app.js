4c7bd9621e8496cf7d9609340ebeccf89b3e506b
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-13 14:52:01 +0100

    START file added

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// console.log('Test')
+
+
+// Visualizzare in pagina 5 numeri casuali con alert.
+// Dopo che l’utente ha chiuso l’alert fate partire un timer di 30 secondi.
+// Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
+
+
+// creare una funzione che generi 5 numeri random e li inserisca dentro un array
+
+// creare un alert mostrando i 5 numeri
+
+// far partire un timer di 30 secondi e poi permettere all'utente di inserire i 5 numeri uno alla volta tramite il prompt, quindi ad ogni inserimento dovremo pushare il numero inserito dentro un array
+
+// confronto tra array - quanti e quali numeri sono stati individuati tramite console.log
