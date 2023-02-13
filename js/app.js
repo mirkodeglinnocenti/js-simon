2ad46f9d71d3b9a6e9acde3b484ff30534beb3e7
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-13 15:53:24 +0100

    FEAT Exercise finished without setTimeout added

--- js/app.js
+++ js/app.js
@@ -8,9 +8,104 @@
 
 
 // creare una funzione che generi 5 numeri random e li inserisca dentro un array
+let arrayNumeri = [];
+let numeroDaInserire;
+
+while ( arrayNumeri.length < 5 ) {
+
+    numeroDaInserire = numeriRandom (1 , 100);
+
+    if ( !arrayNumeri.includes(numeroDaInserire) ) {
+        arrayNumeri.push(numeroDaInserire);
+    } else {
+        continue;
+    }
+
+}
+
+console.log(arrayNumeri)
 
 // creare un alert mostrando i 5 numeri
 
-// far partire un timer di 30 secondi e poi permettere all'utente di inserire i 5 numeri uno alla volta tramite il prompt, quindi ad ogni inserimento dovremo pushare il numero inserito dentro un array
+alert(arrayNumeri);
+
+// far partire un timer di 30 secondi e poi permettere all'utente di inserire i 5 numeri uno alla volta tramite il prompt, 
+// quindi ad ogni inserimento dovremo pushare il numero inserito dentro un array
+
+// setTimeout (function(), 300);
+
+let arrayUtente = [];
+
+for (let i = 0; i < 5; i++){
+
+    let numeroInserito = parseInt(prompt('inserisci numero'));
+
+    arrayUtente.push(numeroInserito);
+}
+
+console.log(arrayUtente)
 
 // confronto tra array - quanti e quali numeri sono stati individuati tramite console.log
+
+let numeriIndividuati = 0;
+
+let numeriInComune = []
+
+for (let i = 0; i < 5; i++){
+    
+    arrayUtente[i];
+
+    // console.log(arrayUtente[i])
+
+    if ( arrayNumeri.includes(arrayUtente[i])) {
+        numeriIndividuati ++;
+        numeriInComune.push(arrayUtente[i]);
+    }
+
+
+}
+
+console.log('Hai indovinato:', numeriIndividuati, 'numeri');
+console.log('Hai indovinato questi numeri:', numeriInComune);
+
+
+
+
+
+
+
+
+
+
+
+// FUNZIONI
+
+// Funzione che genera numeri random
+
+function numeriRandom (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+// function createArray(array, num) {
+//     while ( array.length < 5 ) {
+
+//         num = numeriRandom (1 , 100);
+
+//         if ( !array.includes(num) ) {
+//             array.push(num);
+//         } else {
+//             continue;
+//         }
+
+//     }
+//     return array;
+// }
+
+
+
+
+
+
+
